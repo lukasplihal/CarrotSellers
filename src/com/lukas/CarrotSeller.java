fb14cabebc638771613cdd74206e97d50a5d8757
Lukáš Plíhal <lksplihal@gmail.com>
2025-01-19 22:50:49 +0000

    Initial commit

---
+++ src/com/lukas/CarrotSeller.java
@@ -0,0 +1,81 @@
+
+package com.lukas;
+
+import java.time.LocalDate;
+
+        public class CarrotSellers {
+
+
+            //private String Name;
+            //private LocalDate DateOfBirth;
+            //private int NumberOfContracts;
+            //private double TonsOfCarrot;
+            //private String City;
+            //private String PlateNumber;
+            //private double FuelConsumption;
+
+            private String Name;
+            private LocalDate DateOfBirth;
+            private int NumberOfContracts;
+            private double TonsOfCarrot;
+            private String City;
+            private String PlateNumber;
+            private double FuelConsumption;
+
+            public String getName() {
+                return Name;
+            }
+
+            public void setName(String name) {
+                Name = name;
+            }
+
+            public LocalDate getDateOfBirth() {
+                return DateOfBirth;
+            }
+
+            public void setDateOfBirth(LocalDate dateOfBirth) {
+                DateOfBirth = dateOfBirth;
+            }
+
+            public int getNumberOfContracts() {
+                return NumberOfContracts;
+            }
+
+            public void setNumberOfContracts(int numberOfContracts) {
+                NumberOfContracts = numberOfContracts;
+            }
+
+            public double getTonsOfCarrot() {
+                return TonsOfCarrot;
+            }
+
+            public void setTonsOfCarrot(double tonsOfCarrot) {
+                TonsOfCarrot = tonsOfCarrot;
+            }
+
+            public String getCity() {
+                return City;
+            }
+
+            public void setCity(String city) {
+                City = city;
+            }
+
+            public String getPlateNumber() {
+                return PlateNumber;
+            }
+
+            public void setPlateNumber(String plateNumber) {
+                PlateNumber = plateNumber;
+            }
+
+            public double getFuelConsumption() {
+                return FuelConsumption;
+            }
+
+            public void setFuelConsumption(double fuelConsumption) {
+                FuelConsumption = fuelConsumption;
+            }
+        }
+
